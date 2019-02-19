c23d8647dcd83b145db6c21de0c53033a0432f92
Manish <itzmanish108@gmail.com>
2019-02-19 13:38:45 +0530

    only build directory made available for production on heroku

---
+++ build/precache-manifest.7e0d20e931c7a5e796fd265aae429151.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/static/media/haymaker-webfont.e9535f20.ttf"
+  },
+  {
+    "revision": "0fdbfbdf514d246e7e17",
+    "url": "/static/css/main.2d24d906.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "25f5a9d869864bb03702",
+    "url": "/static/js/2.25f5a9d8.chunk.js"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "0fdbfbdf514d246e7e17",
+    "url": "/static/js/main.0fdbfbdf.chunk.js"
+  },
+  {
+    "revision": "cf798e30eda73d84b25c4ae233fe5e21",
+    "url": "/static/media/haymaker-webfont.cf798e30.svg"
+  },
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "7bbffda6bcacac8360f0ee00bb03dc04",
+    "url": "/static/media/blanch_caps_inline-webfont.7bbffda6.svg"
+  },
+  {
+    "revision": "eda28578064dd1092576b41d9eb5e305",
+    "url": "/index.html"
+  }
+];
--- src/index.js
+++
@@ -1,7 +0,0 @@
-// let's go!
-import React from 'react';
-import { render } from 'react-dom';
-import Router from './components/Router';
-import './css/style.css';
-
-render(<Router />, document.querySelector('#main'));
