d53e3ca46845113db2615793a3ed483466db03d5
Manish <12438068+itzmanish@users.noreply.github.com>
2019-09-23 12:25:30 +0530

    Update base.js

--- src/base.js
+++ src/base.js
@@ -2,9 +2,9 @@ import Rebase from 're-base';
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: 'AIzaSyAJYFTLfKPisPPzvWuyUy_7C5gnz3lsbyw',
-  authDomain: 'prithvi-8baa0.firebaseapp.com',
-  databaseURL: 'https://prithvi-8baa0.firebaseio.com'
+  apiKey: '',
+  authDomain: '',
+  databaseURL: ''
 });
 
 const base = Rebase.createClass(firebaseApp.database());
